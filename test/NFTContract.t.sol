b631014fe642ff2a9f50b5c9935abbf20e7cad68
John <jooohn.eth@gmail.com>
2022-08-22 10:00:48 -0400

    Corrected test implementation

--- test/NFTContract.t.sol
+++ test/NFTContract.t.sol
@@ -2,17 +2,14 @@
 pragma solidity ^0.8.13;
 
 import "../contracts/NFTContract.sol";
+import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
 
-contract NFTContractTest{
+contract NFTContractTest is NFTContract {
 
-    NFTContract nftContract;
-
-    constructor(){
-        nftContract = new NFTContract();
-    }
+    using Counters for Counters.Counter;
 
     function echidna_testMaxSupply() public view returns(bool) {
-        return nftContract.MAX_SUPPLY() <= 100;
+        return tokenIds.current() <= MAX_SUPPLY;
     }
 
 }  
